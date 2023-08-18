06b7ee89e203dfd5c28be45405000baa6ed10846
Nick Grove <nickgrove.se@gmail.com>
2023-08-18 17:15:16 -0400

    Few Small Changes to index/layouts

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,3 @@
-// import jsdom
-const jsdom = require('jsdom')
-
-// create a window with the document object
-const dom = new jsdom.JSDOM('../views/index.ejs')
-
-// import jquery and supply it with the new dom
-const $ = require('jquery')(dom.window)
 const express = require('express')
 const router = express.Router()
 
